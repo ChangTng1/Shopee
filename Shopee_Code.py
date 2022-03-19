2831b4a8b0c878271c0331f7fc7d36f44178e209
itshaihong <77481132+itshaihong@users.noreply.github.com>
2022-03-19 15:40:08 +0800

    Update Shopee_Code.py

--- Shopee_Code.py
+++ Shopee_Code.py
@@ -1,2 +1,20 @@
 print("Hello world")
-print("testing")
+
+#Q2
+import itertools
+
+def weld(bar):
+    possible = []
+    for i in range(len(bar)):
+        this_bar = bar[i]
+        other_bar = bar[:i] + bar[i+1:]
+        for j in range(1, len(other_bar)+1):
+            j_comb = list(itertools.combinations(other_bar, j))
+            for this_set in j_comb:
+
+                if sum(list(this_set)) == this_bar:
+                    possible.append(this_bar)
+    if possible == []:
+        return 0                
+    else:
+        return max(possible)
